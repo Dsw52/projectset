e8594406a897f7074fefa3730f527f5596db0c3e
elizabeth-weyl <elizabeth.weyl@duke.edu>
2025-04-08 01:11:59 -0400

    test

--- week5script.R
+++ week5script.R
@@ -2,4 +2,9 @@
 # Feb 11, 2025
 # Daniel's favorite color is green
 # Daniel's favorite food is plantain or salmon
+<<<<<<< HEAD
 # lizzy's favorite food is tacos and chocolate
+=======
+# sdasdasasdajj
+# lizzy's favorite food is tacos
+>>>>>>> 4ba38092221a9445c4a651cf7ba488e754db3e72
